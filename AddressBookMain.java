b1668a43a501f6702168f27f68e2beec30c70b8f
ToshitaC <toshitachaudhari17@gmail.com>
2021-09-17 22:23:25 +0530

    Added:Program for to creating address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,44 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    public Contacts createContact() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Name:");
+        String firstName = sc.next();
+
+        System.out.println("Enter Last Name:");
+        String lastName = sc.next();
+
+        System.out.println("Enter address:");
+        String address = sc.next();
+
+        System.out.println("Enter city:");
+        String city = sc.next();
+
+        System.out.println("Enter state:");
+        String state = sc.next();
+
+        System.out.println("Enter zip code:");
+        int zip = sc.nextInt();
+
+        System.out.println("Enter phone No.:");
+        String phoneNumber = sc.next();
+
+        System.out.println("Enter email address:");
+        String email = sc.next();
+        sc.close();
+        Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        return contact;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Collection_Address_Book_System_Program..!");
+
+        AddressBookMain addressBookObject = new AddressBookMain();
+        Contacts contact = addressBookObject.createContact();
+        System.out.println("New Contact Created: " + "\n" + contact.toString());
     }
 }
